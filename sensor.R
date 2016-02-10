0dcc0bd402c1cfed89e2e27efda6ee6e8fd462dd
Romeo Kienzler <romeo.kienzler@ch.ibm.com>
2016-02-10 14:32:05 +0100

    IoT meetup

--- sensor.R
+++ sensor.R
@@ -1,31 +1,39 @@
-df1429616120827 <- idaQuery("select * from DASH100527.SENSOR_DATA",as.is=F) 
-#df1429616120827 <- as.data.frame(ida.data.frame('"DASH100527"."SENSOR_DATA"')[ ,c(DEVICEID,TIMESTAMP,VALUE)])
-#df1429626163374 <- as.data.frame(ida.data.frame('"DASH100527"."ALERT"')[ ,c(SENSOR,TIMESTAMP)])
-#df1429616120827$TIMESTAMP
-timestamp=as.numeric(df1429616120827$TIMESTAMP)
-#timestamp
-maxTimestamp=max(timestamp)
-cutOffTimestamp=maxTimestamp - 600
-idx = which(df1429616120827$TIMESTAMP>cutOffTimestamp)
-#idx
-workingSubset=df1429616120827[idx,]
-#workingSubset
-values=as.numeric(workingSubset$VALUE)
-globalMean=mean(values)
-globalSD=sd(values)
+library(ibmdbR)
+mycon <- idaConnect("BLUDB", "", "")
+idaInit(mycon)
 
-devideIds=unique(workingSubset$DEVICEID)
+df1455094435404 <- as.data.frame(ida.data.frame('"DASH105608"."TEMPERATURE"')[ ,c('NAME', 'TEMPERATURE', 'TIMESTAMP')])
+
+
+timestamp=as.numeric(strptime(df1455094435404$TIMESTAMP, "%Y-%m-%d %H:%M:%S.000000"))
+
+
+maxTimestamp=max(timestamp)
+cutOffTimestamp=maxTimestamp - 10
+idx = which(timestamp>cutOffTimestamp)
+#idx
+workingSubset=df1455094435404[idx,]
+#workingSubset
+#values=as.numeric(workingSubset$TEMPERATURE)
+#globalMean=mean(values)
+#globalSD=sd(values)
+
+#globalMean
+#globalSD
+
+devideIds=unique(workingSubset$NAME)
 #devideIds
 scores = 1:length(devideIds)
 for (i in 1:length(devideIds)) {
-    idx = which(workingSubset$DEVICEID==devideIds[i])
+    idx = which(workingSubset$NAME==devideIds[i])
     df=workingSubset[idx,]
-    values=as.numeric(df$VALUE)
-    #scores[i]=sqrt((globalMean+ mean(values))^2) +sqrt((globalSD+ sd(values))^2)
-    scores[i]=abs(globalSD- sd(values))
+    values=as.numeric(df$TEMPERATURE)
+    #scores[i]=(sqrt((globalMean- mean(values))^2) +sqrt((globalSD- sd(values))^2))/2
+    #scores[i]=abs(globalSD- sd(values))
+    scores[i]=sd(values)
 }
 scores
-if (max(scores)>10) {
+if (max(scores)>1) {
 devideIdOutlier=devideIds[scores==max(scores)]
 } else {
 devideIdOutlier=0;
@@ -49,11 +57,10 @@ devideIdOutlier
 #idaUpdate(mycon, updf=df,dfrm=df1429626163374,tablename = '"DASH100527"."ALERT"')
 #idaSave(mycon, dfrm=df1429626163374)
 #idadf(mycon,'insert into DASH100527.ALERT values ("test",0)')
-idadf(mycon,paste('update DASH100527.ALERT set SENSOR=',devideIdOutlier))
+#idadf(mycon,paste('update DASH100527.ALERT set SENSOR=',devideIdOutlier))
 #idadf(mycon,paste('update DASH100527.ALERT set SENSOR=','18'))
 
 
 #plot(df$TIMESTAMP,df$VALUE)
 #plot(Re(fft(as.numeric(df$VALUE)))^2)
-
 
