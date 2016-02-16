a9002c072c73eb8816dfb32340105b76d3eec734
Romeo Kienzler <romeo.kienzler@ch.ibm.com>
2016-02-16 23:16:59 +0100

    xyz

--- sensor.R
+++ sensor.R
@@ -2,17 +2,18 @@ library(ibmdbR)
 mycon <- idaConnect("BLUDB", "", "")
 idaInit(mycon)
 
-df1455094435404 <- as.data.frame(ida.data.frame('"DASH105608"."TEMPERATURE"')[ ,c('NAME', 'TEMPERATURE', 'TIMESTAMP')])
+df <- as.data.frame(ida.data.frame('"DASH110683"."TEMPERATURE"')[ ,c('NAME', 'TEMPERATURE', 'TIMESTAMP')])
 
+pattern="%Y-%m-%d %H:%M:%S.000000"
 
-timestamp=as.numeric(strptime(df1455094435404$TIMESTAMP, "%Y-%m-%d %H:%M:%S.000000"))
+timestamp=as.numeric(strptime(df$TIMESTAMP, pattern))
 
 
 maxTimestamp=max(timestamp)
 cutOffTimestamp=maxTimestamp - 10
 idx = which(timestamp>cutOffTimestamp)
 #idx
-workingSubset=df1455094435404[idx,]
+workingSubset=df[idx,]
 #workingSubset
 #values=as.numeric(workingSubset$TEMPERATURE)
 #globalMean=mean(values)
@@ -43,24 +44,8 @@ devideIdOutlier=0;
 #devideIds
 devideIdOutlier
 
-#f=file(paste('http://noderkie.mybluemix.net/srv?sensor=',devideIdOutlier))
-#read.delim(f)
-
-#channel <- odbcConnect("BLUDB",believeNRows=FALSE)
-#df = data.frame(matrix(vector(length=2), 1, 2, dimnames=list(c(), c("SENSOR", "TIMESTAMP"))))
-#df1429626163374$SENSOR[1]=devideIdOutlier
-#df1429626163374$TIMESTAMP[1]=Sys.time()
-#df$SENSOR[1]=devideIdOutlier
-#df$TIMESTAMP[1]=Sys.time()
-#df
-#sqlSave(channel, df1429626163374, tablename = '"DASH100527"."ALERT2"')
-#idaUpdate(mycon, updf=df,dfrm=df1429626163374,tablename = '"DASH100527"."ALERT"')
-#idaSave(mycon, dfrm=df1429626163374)
-#idadf(mycon,'insert into DASH100527.ALERT values ("test",0)')
-#idadf(mycon,paste('update DASH100527.ALERT set SENSOR=',devideIdOutlier))
-#idadf(mycon,paste('update DASH100527.ALERT set SENSOR=','18'))
-
-
-#plot(df$TIMESTAMP,df$VALUE)
-#plot(Re(fft(as.numeric(df$VALUE)))^2)
+#dfalert <- as.data.frame(ida.data.frame('"DASH110683"."alert"')[ ,c('sensor')])
+#dfalert
+#idaUpdate(mycon, dfrm=dfalert)
+idadf(mycon,paste('update DASH110683.alert set sensor=\'',devideIdOutlier,'\''))
 
