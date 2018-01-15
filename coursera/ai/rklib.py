47d50178694080e1d7410a67bacc01748bef8c4f
Romeo Kienzler <romeo.kienzler@gmail.com>
2018-01-15 11:45:12 +0100

    submit key and part

--- coursera/ai/rklib.py
+++ coursera/ai/rklib.py
@@ -1,13 +1,13 @@
 import requests, json
 
-def submit(submitterEmail,secret):
+def submit(submitterEmail,secret,key,part):
         with open('model.h5.base64', 'r') as myfile:
             data=myfile.read()
         submission = {
-                    "assignmentKey": "XbAMqtjdEeepUgo7OOVwng",
+                    "assignmentKey": key,
                     "submitterEmail":  submitterEmail,
                     "secret":  secret,
-                    "parts": {"LqPRQ" : {"output": data}}
+                    "parts": {part : {"output": data}}
                   }
         response = requests.post('https://www.coursera.org/api/onDemandProgrammingScriptSubmissions.v1', data=json.dumps(submission))
         if response.status_code == 201:
