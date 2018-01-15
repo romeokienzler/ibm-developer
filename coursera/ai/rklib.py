f0aaafc5c960f1cc2918d1678ec23bc8967b4ce2
Romeo Kienzler <romeo.kienzler@gmail.com>
2018-01-15 23:30:37 +0100

    multipart support added

--- coursera/ai/rklib.py
+++ coursera/ai/rklib.py
@@ -1,12 +1,17 @@
 import requests, json
 
-def submit(submitterEmail,secret,key,part, data):
+def submit(submitterEmail,secret,key,submission_part, all_parts, data):
         submission = {
                     "assignmentKey": key,
                     "submitterEmail":  submitterEmail,
                     "secret":  secret,
-                    "parts": {part : {"output": data}}
+                    "parts": {}
                   }
+        for part in all_parts:
+            if part == submission_part:
+                submission["parts"][part] = {"output": data}
+            else:
+                submission["parts"][part] = dict()
         response = requests.post('https://www.coursera.org/api/onDemandProgrammingScriptSubmissions.v1', data=json.dumps(submission))
         if response.status_code == 201:
             print "Submission successful, please check on the coursera grader page for the status"
