2b582d856d153f7d6b51c1eb965d91ecd5304107
Romeo Kienzler <romeo.kienzler@gmail.com>
2018-01-30 11:57:00 +0100

    added support for multi part submit

--- coursera/ai/rklib.py
+++ coursera/ai/rklib.py
@@ -20,3 +20,21 @@ def submit(submitterEmail,secret,key,submission_part, all_parts, data):
             print ("-------------------------")
             print (response.text)
             print ("-------------------------")
+            
+def submitAll(submitterEmail,secret,key,parts_and_data):
+        submission = {
+                    "assignmentKey": key,
+                    "submitterEmail":  submitterEmail,
+                    "secret":  secret,
+                    "parts": {}
+                  }
+        for part in parts_and_data:
+            submission["parts"][part] = {"output": parts_and_data[part]}
+        response = requests.post('https://www.coursera.org/api/onDemandProgrammingScriptSubmissions.v1', data=json.dumps(submission))
+        if response.status_code == 201:
+            print ("Submission successful, please check on the coursera grader page for the status")
+        else:
+            print ("Something went wrong, please have a look at the reponse of the grader")
+            print ("-------------------------")
+            print (response.text)
+            print ("-------------------------")
